8e4456a329d8118d0f6719ceec9d60e673e17dfe
marcopolomoreno <marcopolo@unir.br>
2024-11-29 19:55:53 -0400

    Initial Commit

---
+++ simufisica-tv-os-demoTests/simufisica_tv_os_demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  simufisica_tv_os_demoTests.swift
+//  simufisica-tv-os-demoTests
+//
+//  Created by Marco Polo on 29/11/24.
+//
+
+import Testing
+@testable import simufisica_tv_os_demo
+
+struct simufisica_tv_os_demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ simufisica-tv-os-demoUITests/simufisica_tv_os_demoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  simufisica_tv_os_demoUITestsLaunchTests.swift
+//  simufisica-tv-os-demoUITests
+//
+//  Created by Marco Polo on 29/11/24.
+//
+
+import XCTest
+
+final class simufisica_tv_os_demoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
