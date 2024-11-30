7b42e2b05e418b6450b7157842dd935802b60d0d
marcopolomoreno <marcopolo@unir.br>
2024-11-29 20:04:36 -0400

    commit

--- simufisica-tv-os-demo/ViewController.swift
+++ simufisica-tv-os-demo/ViewController.swift
@@ -1,19 +1,44 @@
 //
 //  ViewController.swift
-//  simufisica-tv-os-demo
+//  teste
 //
-//  Created by Marco Polo on 29/11/24.
+//  Created by Marco Polo on 27/11/24.
 //
 
+import AVKit
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "demo", ofType: "mp4")!))
+        let layer = AVPlayerLayer(player: player)
+        layer.frame = view.bounds
+        layer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(layer)
+        //player.volume = 0
+        player.play()
+        player.actionAtItemEnd = .none // Impede a pausa no final
+        
+        // Loop
+        NotificationCenter.default.addObserver(
+            forName: .AVPlayerItemDidPlayToEndTime,
+            object: player.currentItem,
+            queue: .main
+        ) { _ in
+            player.seek(to: .zero)
+            player.play()
+        }
+        
     }
 
 
 }
-
