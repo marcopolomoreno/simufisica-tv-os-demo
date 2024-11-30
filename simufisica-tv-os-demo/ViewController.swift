13039cbc99509bd3e604c7ce7eae8d48257e07a3
marcopolomoreno <marcopolo@unir.br>
2024-11-29 20:20:25 -0400

    video

--- simufisica-tv-os-demo/ViewController.swift
+++ simufisica-tv-os-demo/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "demo", ofType: "mp4")!))
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "simufisica", ofType: "mp4")!))
         let layer = AVPlayerLayer(player: player)
         layer.frame = view.bounds
         layer.videoGravity = .resizeAspectFill
